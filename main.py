932a58360ac77e233a7e9b0d10cd23a42cd2c6e3
Ben Governali <ben.governali@ey.com>
2024-02-07 12:45:55 -0800

    added comparison logic and printing of score

--- main.py
+++ main.py
@@ -21,26 +21,46 @@ def higher_lower():
     #print the logo for the game
     print(art.logo)
 
-    #set the looping condition
+    #set the game data, conditions, and initial item
     game_running = True
-
-    #set initial game data
     data = game_data.data
-
-    #set initial item and update game_data
     item_a, data = select_item(data)
+    score = 0
     
     #start while loop for game
     while game_running:
+        #check if there are any entries left to compare
+        if len(data) == 0:
+            print("You have compared all celebrities, you are a guessing genius!")
+            game_running = False
+        
         #select a random entry from the game data as second value for comparison
         item_b, data = select_item(data)
-        print(item_a)
-        print(item_b)
-        game_running = False
 
-    #end game
-    return
-        
-        
+        #display the search terms to compare
+        print(f"Compare A: {item_a['name']}, a {item_a['description']} from {item_a['country']}.")
+        print(art.vs)
+        print(f"Against B: {item_b['name']}, a {item_b['description']} from {item_b['country']}.")
+
+        #ask the user to guess
+        guess = input("Who has more followers? Type 'A' or 'B': ")
+
+        #set the user's guess to one of the items
+        if guess == "A":
+            guess_item = item_a
+            compare_item = item_b
+        else:
+            #there should be additional logic here for handling an invalid input
+            guess_item = item_b
+            compare_item = item_a
+
+        #compare follower counts for guess vs compare
+        if guess_item["follower_count"] > compare_item["follower_count"]:
+            score += 1
+            print(f"You're right! Current score: {score}")
+            item_a = guess_item
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            game_running = False        
 
 higher_lower()
